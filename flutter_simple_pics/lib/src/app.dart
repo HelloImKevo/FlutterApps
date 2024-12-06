a425df071eec5cfb2053e328005daa8f9c2e4ff6
Kevin Schanz <kevinschanz@live.com>
2024-12-06 18:37:50 -0500

    [Simple Pics] Refactor to use StatefulWidget and increment Counter var

--- flutter_simple_pics/lib/src/app.dart
+++ flutter_simple_pics/lib/src/app.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return HomeScreenState();
+  }
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  int counter = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -10,10 +19,13 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(AppLocalizations.of(context)!.seeSomeImages),
       ),
+      body: Text('$counter'),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
-          print('Hi there!');
+          setState(() {
+            counter += 1;
+          });
         },
       ),
     );
