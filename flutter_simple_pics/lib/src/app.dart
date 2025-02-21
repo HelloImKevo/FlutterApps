2e236e9d30867e15eccb33c4d05440307396f910
Kevin Schanz <kevinschanz@live.com>
2025-02-21 18:38:56 -0500

    [Simple Pics] Add 'logger' dependency and SSLHandshakeException Certificate Workaround for Debug Builds

--- flutter_simple_pics/lib/src/app.dart
+++ flutter_simple_pics/lib/src/app.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:logger/logger.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' show json;
 import 'models/image_model.dart';
+import 'widgets/image_list.dart';
+
+final logger = Logger();
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -20,9 +24,10 @@ class HomeScreenState extends State<HomeScreen> {
   void fetchImage() async {
     counter++;
 
+    String url = 'https://jsonplaceholder.typicode.com/photos/$counter';
+
     try {
-      http.Response response = await http.get(
-          Uri.parse('https://jsonplaceholder.typicode.com/photos/$counter'));
+      http.Response response = await http.get(Uri.parse(url));
 
       if (response.statusCode != 200) {
         showSnackBar('Failed to load image');
@@ -30,6 +35,8 @@ class HomeScreenState extends State<HomeScreen> {
       }
 
       var imageModel = ImageModel.fromJson(json.decode(response.body));
+
+      logger.d("Image loaded: ${imageModel.url}");
 
       // This function updates the state of the widget by adding a new image to the
       // list of images. It is necessary to call this function whenever a new image
@@ -39,11 +46,13 @@ class HomeScreenState extends State<HomeScreen> {
         images.add(imageModel);
       });
     } catch (e) {
-      showSnackBar('Error: $e');
+      showSnackBar('Error loading $url\n$e');
     }
   }
 
   void showSnackBar(String message) {
+    logger.w(message);
+
     // Check if the widget is still mounted before attempting to show a SnackBar.
     // This is necessary because the fetchImage function is asynchronous and the
     // widget might have been disposed of before the function completes.
@@ -60,7 +69,7 @@ class HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: Text(AppLocalizations.of(context)!.seeSomeImages),
       ),
-      body: Text('$counter'),
+      body: ImageList(images: images),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: fetchImage,
